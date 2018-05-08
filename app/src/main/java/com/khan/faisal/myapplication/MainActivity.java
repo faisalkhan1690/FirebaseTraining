a4cd0be66f773884edd542891747c4ccc6ffd145
Faisal Khan <faisal.khan@kelltontech.com>
2018-05-08 19:07:26 +0530

    final commit

--- app/src/main/java/com/khan/faisal/myapplication/MainActivity.java
+++ app/src/main/java/com/khan/faisal/myapplication/MainActivity.java
@@ -3,32 +3,26 @@ package com.khan.faisal.myapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
-import com.google.android.gms.analytics.HitBuilders;
-import com.google.android.gms.analytics.Tracker;
-import com.google.firebase.analytics.FirebaseAnalytics;
-
-import java.util.HashMap;
+import com.google.analytics.tracking.android.Fields;
+import com.google.analytics.tracking.android.GoogleAnalytics;
+import com.google.analytics.tracking.android.MapBuilder;
+import com.google.analytics.tracking.android.Tracker;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Tracker mTracker;
 
-    private final String mEventLogin="Event_Login";
-    private final String mEventLogout="Event_Logout";
-    private final String mEventPlanView="Event_Plan_View";
-    private final String mEventPlanBuy="Event_Plan_Buy";
+    private final String mScreenLogin="Login_Screen";
+    private final String mScreenLogout="Logout_Screen";
+    private final String mScreenPlanView="Plan_View_Screen";
+    private final String mScreenPlanBuy="Plan_Buy_Screen";
 
 
-    private final String mKeySalesPerson="Sales_Person_Id";
-    private final String mKeyCustomerId="Customer_Id";
-    private final String mKeyPlanID="Plan_Id";
-
-
-
-    private final String mSalesPersonId="sales@test.com";
-    private final String mCustomerId="customer@test.com";
+    private String mSalesPersonId="sales@test.com";
+    private String mCustomerId="customer@test.com";
     private final String mPlanOne="Plan_One";
     private final String mPlanTwo="Plan_Three";
     private final String mPlanThree="Plan_Three";
@@ -38,113 +32,67 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AnalyticsApplication application = (AnalyticsApplication) getApplication();
-        mTracker = application.getDefaultTracker();
+        mTracker = GoogleAnalytics.getInstance(this).getDefaultTracker();
 
     }
 
 
     public void loginUser(View view){
+        EditText etSalesPersonId=findViewById(R.id.et_sales_person_id);
+        EditText eCustomerId=findViewById(R.id.et_customer_id);
 
-        HashMap<String,String> dataMap=new HashMap<>();
-        dataMap.put(mKeySalesPerson, mSalesPersonId);
-        dataMap.put(mKeyCustomerId, mCustomerId);
-        mTracker.send(new HitBuilders.EventBuilder()
-                .setCategory(mEventLogin)
-                .setAction(mEventLogin)
-                .setLabel(mEventLogin)
-                .setAll(dataMap)
-                .build());
+        mSalesPersonId=etSalesPersonId.getText().toString();
+        mSalesPersonId=eCustomerId.getText().toString();
+
+        mTracker.set(Fields.customDimension(1), mSalesPersonId);
+        mTracker.set(Fields.customDimension(2), mCustomerId);
+        mTracker.set(Fields.customDimension(3), null);
+        mTracker.set(Fields.SCREEN_NAME, mScreenLogin);
+        mTracker.send(MapBuilder.createAppView().build());
     }
 
     public void logout(View view){
-        HashMap<String,String> dataMap=new HashMap<>();
-        dataMap.put(mKeySalesPerson, mSalesPersonId);
-        dataMap.put(mKeyCustomerId, mCustomerId);
-        mTracker.send(new HitBuilders.EventBuilder()
-                .setCategory(mEventLogout)
-                .setAction(mEventLogout)
-                .setLabel(mEventLogout)
-                .setAll(dataMap)
-                .build());
+        mTracker.set(Fields.customDimension(1), mSalesPersonId);
+        mTracker.set(Fields.customDimension(2), mCustomerId);
+        mTracker.set(Fields.customDimension(3), null);
+        mTracker.set(Fields.SCREEN_NAME, mScreenLogout);
+        mTracker.send(MapBuilder.createAppView().build());
     }
 
     public void planViewOne(View view){
-        HashMap<String,String> dataMap=new HashMap<>();
-        dataMap.put(mKeySalesPerson, mSalesPersonId);
-        dataMap.put(mKeyCustomerId, mCustomerId);
-        dataMap.put(mKeyPlanID, mPlanOne);
-        mTracker.send(new HitBuilders.EventBuilder()
-                .setCategory(mEventPlanView)
-                .setAction(mEventPlanView)
-                .setLabel(mEventPlanView)
-                .setAll(dataMap)
-                .build());
+        mTracker.set(Fields.customDimension(3), mPlanOne);
+        mTracker.set(Fields.SCREEN_NAME, mScreenPlanView);
+        mTracker.send(MapBuilder.createAppView().build());
     }
 
     public void planViewTwo(View view){
-        HashMap<String,String> dataMap=new HashMap<>();
-        dataMap.put(mKeySalesPerson, mSalesPersonId);
-        dataMap.put(mKeyCustomerId, mCustomerId);
-        dataMap.put(mKeyPlanID, mPlanTwo);
-        mTracker.send(new HitBuilders.EventBuilder()
-                .setCategory(mEventPlanView)
-                .setAction(mEventPlanView)
-                .setLabel(mEventPlanView)
-                .setAll(dataMap)
-                .build());
+        mTracker.set(Fields.customDimension(3), mPlanTwo);
+        mTracker.set(Fields.SCREEN_NAME, mScreenPlanView);
+        mTracker.send(MapBuilder.createAppView().build());
 
     }
     public void planViewThree(View view){
-        HashMap<String,String> dataMap=new HashMap<>();
-        dataMap.put(mKeySalesPerson, mSalesPersonId);
-        dataMap.put(mKeyCustomerId, mCustomerId);
-        dataMap.put(mKeyPlanID, mPlanThree);
-        mTracker.send(new HitBuilders.EventBuilder()
-                .setCategory(mEventPlanView)
-                .setAction(mEventPlanView)
-                .setLabel(mEventPlanView)
-                .setAll(dataMap)
-                .build());
+        mTracker.set(Fields.customDimension(3), mPlanThree);
+        mTracker.set(Fields.SCREEN_NAME, mScreenPlanView);
+        mTracker.send(MapBuilder.createAppView().build());
 
     }
     public void planBuyOne(View view){
-        HashMap<String,String> dataMap=new HashMap<>();
-        dataMap.put(mKeySalesPerson, mSalesPersonId);
-        dataMap.put(mKeyCustomerId, mCustomerId);
-        dataMap.put(mKeyPlanID, mPlanOne);
-        mTracker.send(new HitBuilders.EventBuilder()
-                .setCategory(mEventPlanBuy)
-                .setAction(mEventPlanBuy)
-                .setLabel(mEventPlanBuy)
-                .setAll(dataMap)
-                .build());
+        mTracker.set(Fields.customDimension(3), mPlanOne);
+        mTracker.set(Fields.SCREEN_NAME, mScreenPlanBuy);
+        mTracker.send(MapBuilder.createAppView().build());
 
     }
     public void planBuyTwo(View view){
-        HashMap<String,String> dataMap=new HashMap<>();
-        dataMap.put(mKeySalesPerson, mSalesPersonId);
-        dataMap.put(mKeyCustomerId, mCustomerId);
-        dataMap.put(mKeyPlanID, mPlanTwo);
-        mTracker.send(new HitBuilders.EventBuilder()
-                .setCategory(mEventPlanBuy)
-                .setAction(mEventPlanBuy)
-                .setLabel(mEventPlanBuy)
-                .setAll(dataMap)
-                .build());
+        mTracker.set(Fields.customDimension(3), mPlanTwo);
+        mTracker.set(Fields.SCREEN_NAME, mScreenPlanBuy);
+        mTracker.send(MapBuilder.createAppView().build());
 
     }
     public void planBuyThree(View view){
-        HashMap<String,String> dataMap=new HashMap<>();
-        dataMap.put(mKeySalesPerson, mSalesPersonId);
-        dataMap.put(mKeyCustomerId, mCustomerId);
-        dataMap.put(mKeyPlanID, mPlanThree);
-        mTracker.send(new HitBuilders.EventBuilder()
-                .setCategory(mEventPlanBuy)
-                .setAction(mEventPlanBuy)
-                .setLabel(mEventPlanBuy)
-                .setAll(dataMap)
-                .build());
+        mTracker.set(Fields.customDimension(3), mPlanThree);
+        mTracker.set(Fields.SCREEN_NAME, mScreenPlanBuy);
+        mTracker.send(MapBuilder.createAppView().build());
 
     }
 }
